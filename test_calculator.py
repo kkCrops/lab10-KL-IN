aee462145a6f2d7a96eaa0020b8b7e80b76878d7
IN <Izzy@isabellas-MacBook-Air.local>
2025-04-13 19:56:03 -0400

    Part 2 Partner 2

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,15 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
     # Partner 2
     def test_add(self): # 3 assertions
-        pass
+        self.assertEqual(add(10,3), 13)
+        self.assertEqual(add(-1, -1), -2)
+        self.assertNotEqual(add(0, 5), 0)
+
 
     def test_subtract(self): # 3 assertions
-        pass
-    ##########################
-
+        self.assertEqual(sub(10, 3), 7)
+        self.assertEqual(sub(-5, -7), 2)
+        self.assertNotEqual(sub(0, 5), 5)
     # Partner 1
     def test_multiply(self): # 3 assertions
         pass
@@ -19,14 +22,16 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
     # Partner 2
-    def test_divide_by_zero(self): # 1 assertion
-        # call division function inside, example:
-        # with self.assertRaises(<INSERT_ERROR_TYPE>):
-        #     div(0, 5)
-        pass
+    def test_divide_by_zero(self):# 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 0)
+            div(0, -10)
+            div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        pass
+        self.assertEqual(logarithm(10, 1), 0)
+        self.assertEqual(logarithm(47, 103823), 3)
+        self.assertNotEqual(logarithm(10, 10), 10)
 
     def test_log_invalid_base(self): # 1 assertion
         # use same technique from test_divide_by_zero
@@ -34,11 +39,15 @@ class TestCalculator(unittest.TestCase):
     ##########################
     
     # Partner 1
-    def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
+            logarithm(1, 10)
+            logarithm(-5, 5)
+        # 1 assertion
         # call log function inside, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #     logarithm(0, 5)
-        pass
 
     def test_hypotenuse(self): # 3 assertions
         pass
